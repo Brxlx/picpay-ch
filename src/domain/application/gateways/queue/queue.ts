2a2e1687a4d3ca77cbe77f4af1cd6c5161275b1a
Bruno Santos <brcesar19@gmail.com>
2024-12-19 21:25:00 -0300

    docs(queue): add description from base queue class

--- src/domain/application/gateways/queue/queue.ts
+++ src/domain/application/gateways/queue/queue.ts
@@ -1,3 +1,6 @@
+/**
+ * Abstract base class for a queue implementation, providing methods to produce and consume messages.
+ */
 export abstract class Queue {
   abstract produce(topic: string | string[], message: Buffer): Promise<void>;
   abstract consume(topic: string): Promise<void>;
